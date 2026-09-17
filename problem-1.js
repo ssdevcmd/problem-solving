e17d5b16221a0412d9198c68b4ddf2d1561d3135
ssdevcmd <ssph079@gmail.com>
2026-09-17 09:10:17 +0600

    solved problem-1 using destructuring & arithmetic operators

--- problem-1.js
+++ problem-1.js
@@ -1,3 +1,11 @@
+// Problem 1: Swap Two Variables  [Easy]
+//Description: Write a function that swaps the values of two variables without using a third variable.
+// Example:
+// Input: a = 5, b = 10; Output: a = 10, b = 5
+// Hint: Try using destructuring or arithmetic operators.
+
+ [destructuring] 
+
 let a = 5;
 let b = 10;
 
@@ -5,3 +13,14 @@ let b = 10;
 
 console.log(a);
 console.log(b);
+
+[arithmetic]
+
+let c = 5;
+let d = 10;
+
+c += 5;
+d -= 5;
+
+console.log(c);
+console.log(d);
