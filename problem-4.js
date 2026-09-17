b36b943449a0d6c83d75aec4e3d139275c95a13b
ssdevcmd <ssph079@gmail.com>
2026-09-17 10:10:21 +0600

    solved problem-4 using fahrenheit to celcius formula

---
+++ problem-4.js
@@ -0,0 +1,12 @@
+//Problem 4: Celsius to Fahrenheit  [Easy]
+//Description: Write a function toFahrenheit(celsius) that converts a Celsius temperature to Fahrenheit.
+//Example:
+//Input: 0   → Output: 32; Input: 100 → Output: 212
+//Hint: Formula: (C × 9/5) + 32
+
+function toFahrenheit(celsius){
+    return (celsius * 9/5) +32
+}
+
+console.log(toFahrenheit(0));
+console.log(toFahrenheit(100));
