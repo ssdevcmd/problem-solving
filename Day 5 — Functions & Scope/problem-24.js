7f049849ee4a5e4b38453f1f2bb1e6968fa2174f
ssdevcmd <ssph079@gmail.com>
2026-09-22 12:30:12 +0600

    solved problem-23.js using Curried function & Closures method

---
+++ Day 5 — Functions & Scope/problem-24.js
@@ -0,0 +1,18 @@
+//Problem 24: Curry a Function  [Medium]
+//Description: Write a function curry(fn) that converts a function of two arguments into a curried version.
+//Example:
+//const add = curry((a,b) => a+b);add(2)(3); // 5
+//Hint: Return a function from inside a function.
+
+
+function curry(fn){
+    return function(a){
+        return function(b){
+            return fn(a, b);
+        }
+    }
+}
+
+const add = curry((a, b) => a + b);
+const result = add(2)(3);
+console.log(result); // 5
