a5ee5513aa6a67c8cd125a0e0c80b69e4d685acd
ssdevcmd <ssph079@gmail.com>
2026-09-22 22:15:53 +0600

    solved problem-26 using Destructure method

---
+++ Day 6 — ES6+ Features/problem-26.js
@@ -0,0 +1,11 @@
+//Problem 26: Destructure and Rename  [Easy]
+//Description: Given a user object, use destructuring to extract firstName, lastName, and age, renaming firstName to name.
+//Example:
+//const user = {firstName:'Sara', lastName:'Khan', age:25};// Extract: name='Sara', lastName='Khan', age=25
+//Hint: Use: const { firstName: name, lastName, age } = user;
+
+const user = {firstName: 'Sara', lastName: 'Khan', age: 25};
+const { firstName: name, lastName, age } = user;
+console.log(name); // Output: Sara
+console.log(lastName); // Output: Khan
+console.log(age); // Output: 25
