65fd4f829c0995bb0a43e55867b339595e4e48f0
ssdevcmd <ssph079@gmail.com>
2026-09-23 08:20:24 +0600

    solved problem-30 using optional chining(?.) and nullish coalescing(??)

---
+++ Day 6 — ES6+ Features/problem-30.js
@@ -0,0 +1,16 @@
+//Problem 30: Optional Chaining & Nullish Coalescing  [Easy]
+//Description: Given a nested object that may have missing properties, safely access a deeply nested value using optional chaining (?.) and provide a default using nullish coalescing (??).
+//Example:
+//const user = {profile: null};const city = user?.profile?.address?.city ?? 'Unknown';// Output: 'Unknown'
+//Hint: Chain ?. for each level; use ?? for the fallback.
+
+
+const user1 = {profile: null}
+const user2 = {profile: {address: {city: 'Dhaka'}}}
+
+function getCityName(user){
+    return user?.profile?.address?.city?? 'Unknown';
+}
+
+console.log(getCityName(user1));
+console.log(getCityName(user2));
