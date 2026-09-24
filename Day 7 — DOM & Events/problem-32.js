d899104b32da7edaab6d4f5f8e65cb106a6f5a99
ssdevcmd <ssph079@gmail.com>
2026-09-24 15:18:19 +0600

    solved problem-32 using Throttle Function

---
+++ Day 7 — DOM & Events/problem-32.js
@@ -0,0 +1,19 @@
+//Problem 32: Throttle Function  [Medium]
+//Description: Write a throttle(fn, limit) function that ensures fn is called at most once every limit milliseconds.
+//Example:
+//const throttledScroll = throttle(onScroll, 200);// Fires at most once every 200ms during scroll
+//Hint: Track the last call time with Date.now().
+
+
+function throttle(fn, limit){
+    let lastCallTime = 0;
+
+    return function(){
+        const now = Date.now();
+        if(now - lastCallTime >= limit){
+            lastCallTime = now;
+            fn.apply(this, arguments);
+        }
+    }
+    
+}
